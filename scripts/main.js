8ac6a0d89b8f20806a6c1837c9035ea2938dcc83
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2023-05-12 16:37:03 +0300

    Added WebWorker

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-import { currentlyValues, moreNineTimes } from './utils.js';
-
 const timerBtn = document.querySelector('.timer-btn'); // timer start button
 
 const numberHours = document.querySelector('.number-hours'); // number that shows how many hours
@@ -17,53 +15,10 @@ const lapBtn = document.querySelector('.lap'); // lap button
 
 const timerCount = document.querySelector('.currently-timer'); // time is displayed here
 
-// storage
-let interval; // variable for start and reset setInterval
-let lapCount = 0; // number of laps
-let milliseconds = 0;
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+let worker = new Worker('./scripts/worker.js');
 
-// adding 1 milliseconds
-const tick = () => {
-    milliseconds++;
-
-    // get actual value and show in title
-    let values = currentlyValues(hours,minutes,seconds,milliseconds);
-    document.title = `Time: ${values.hoursValue} : ${values.minutesValue} : ${values.secondsValue}`;
-    
-    // logic to convert milliseconds
-    moreNineTimes(milliseconds,numberMilliseconds);
-    if(milliseconds === 100) {
-        milliseconds = 0;
-        seconds++;
-    }
-
-    // logic to convert seconds
-    moreNineTimes(seconds,numberSeconds);
-    if(seconds === 60) {
-        seconds = 0;
-        minutes++;
-    }
-
-    // logic to convert minutes
-    moreNineTimes(minutes,numberMinutes);
-    if(minutes === 60) {
-        minutes = 0;
-        hours++;
-    }
-
-    // logic to convert hours
-    moreNineTimes(hours,numberHours);
-}
-
-// Function start stopwatch
 const start = () => {
-    // clear and start a new stopwatch if it is not running
-    clearInterval(interval);
-    interval = setInterval(tick, 10);
-    // show/hide elements
+    worker.postMessage('start');
     timerBtn.remove();
     helpersBtns.style.display = 'flex';
     timerCount.style.display = 'flex';
@@ -73,31 +28,19 @@ const start = () => {
     playBtn.style.display = 'none';
 }
 
-// Function stop stopwatch
 const stop = () => {
-    // clear the stopwatch
-    clearInterval(interval);
-    // show/hide elements
+    worker.postMessage('stop');
     playBtn.style.display = 'flex';
     stopBtn.style.display = 'none';
 }
 
-// Function reset stopwatch
 const reset = () => {
-    // clear the stopwatch
-    clearInterval(interval);
-    // reset stopwatch values
+    worker.postMessage('reset');
     numberMilliseconds.innerHTML = '00';
     numberSeconds.innerHTML = '00';
     numberMinutes.innerHTML = '00';
     numberHours.innerHTML = '00';
-    milliseconds = 0;
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-    lapCount = 0;
     document.title = "Stopwatch";
-    // show/hide elements
     lapResultsList.replaceChildren();
     playBtn.style.display = 'flex';
     lapResultsBlock.style.display = 'none';
@@ -106,39 +49,44 @@ const reset = () => {
     resetBtn.style.display = 'none';
 }
 
-// Function lap stopwatch
 const lap = () => {
-    // add plus 1 to the number of laps
-    lapCount++;
-    // show a block with a list of laps
+    worker.postMessage('lap');
     lapResultsBlock.style.display = 'block';
-
-    // if there are more than 100 or 1000 circles, then add a class so that the number of laps is displayed correctly
-    if(lapCount === 100) {
-        lapResultsList.classList.add("over-100");
-    }
-    if (lapCount === 1000) {
-        lapResultsList.classList.add("over-1000");
-        lapResultsList.classList.remove("over-100");
-    }
-
-    // get the actual time value
-    let values = currentlyValues(hours,minutes,seconds,milliseconds);
-
-    // create a layout for displaying laps on the page
-    let htmlContent = `<li class="results-item timer-count">
-                            <p class="lap-count">lap <span>${lapCount}</span></p>
-                            <span class="number-hours">${values.hoursValue}</span><span>:</span>
-                            <span class="number-minutes">${values.minutesValue}</span><span>:</span>
-                            <span class="number-seconds">${values.secondsValue}</span><span>:</span>
-                            <span class="number-milliseconds">${values.millisecondsValue}</span>
-                        </li>`;
-    
-    // insert a lap layout on the page
-    lapResultsList.insertAdjacentHTML('beforeend', htmlContent);
 }
 
-// start timer on button click
+worker.onmessage = function(e) {
+    const { millisecondsValue, secondsValue, minutesValue, hoursValue, lapCount, isLap } = e.data;
+    
+    numberMilliseconds.innerHTML = millisecondsValue;
+    numberSeconds.innerHTML = secondsValue;
+    numberMinutes.innerHTML = minutesValue;
+    numberHours.innerHTML = hoursValue;
+
+    document.title = `Time: ${hoursValue} : ${minutesValue} : ${secondsValue}`;
+
+    if(isLap === true) {
+        worker.postMessage('lapStop');
+        // if there are more than 100 or 1000 circles, then add a class so that the number of laps is displayed correctly
+        if(lapCount === 100) {
+            lapResultsList.classList.add("over-100");
+        }
+        if (lapCount === 1000) {
+            lapResultsList.classList.add("over-1000");
+            lapResultsList.classList.remove("over-100");
+        }
+
+        let htmlContent = `<li class="results-item timer-count">
+                                        <p class="lap-count">lap <span>${lapCount}</span></p>
+                                        <span class="number-hours">${hoursValue}</span><span>:</span>
+                                        <span class="number-minutes">${minutesValue}</span><span>:</span>
+                                        <span class="number-seconds">${secondsValue}</span><span>:</span>
+                                        <span class="number-milliseconds">${millisecondsValue}</span>
+                                    </li>`;
+
+        lapResultsList.insertAdjacentHTML('beforeend', htmlContent);
+    }
+};
+
 timerBtn.addEventListener('click', start);
 playBtn.addEventListener('click', start);
 stopBtn.addEventListener('click', stop);
---
+++ scripts/worker.js
@@ -0,0 +1,96 @@
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let lapCount = 0;
+let isLap = false;
+let interval;
+
+const currentlyValues = (hours,minutes,seconds,milliseconds) => {
+    let millisecondsValue = milliseconds > 9 ? milliseconds : '0' + milliseconds;
+    let secondsValue = seconds > 9 ? seconds : '0' + seconds;
+    let minutesValue = minutes > 9 ? minutes : '0' + minutes;
+    let hoursValue = hours > 9 ? hours : '0' + hours;
+    let values = {
+        isLap,
+        lapCount,
+        hoursValue,
+        minutesValue,
+        secondsValue,
+        millisecondsValue,
+    }
+    return values;
+}
+
+const tick = () => {
+    milliseconds++;
+
+    if(milliseconds >= 100) {
+        milliseconds = 0;
+        seconds++;
+    }
+
+    if(seconds >= 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    if(minutes >= 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    let values = currentlyValues(hours,minutes,seconds,milliseconds);
+
+    this.postMessage(values);
+}
+
+const startTimer = () => {
+    clearInterval(interval);
+    interval = setInterval(tick, 10);
+}
+
+const stopTimer = () => {
+    clearInterval(interval);
+}
+
+const resetTimer = () => {
+    clearInterval(interval);
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    lapCount = 0;
+    isLap = false;
+}
+
+const lapTimer = () => {
+    lapCount++;
+    isLap = true;
+    let values = currentlyValues(hours,minutes,seconds,milliseconds);
+    this.postMessage(values);
+}
+
+const lapStopTimer = () => {
+    isLap = false;
+}
+
+this.addEventListener('message', function(event) {
+    switch(event.data) {
+        case "start":
+            startTimer();
+            break;
+        case "stop":
+            stopTimer();
+            break;
+        case "reset":
+            resetTimer();
+            break;
+        case "lap":
+            lapTimer();
+            break;
+        case "lapStop":
+            lapStopTimer();
+            break;
+    }
+});
